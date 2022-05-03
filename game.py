684d8d77f3119d51e8f2b72a4ab2bf5bb6f3d341
EPIXsl <104023799+EPIXsl@users.noreply.github.com>
2022-05-03 18:52:25 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,15 +1,13 @@
 from pygame import *
 
 class GameSprite(sprite.Sprite):
-    #конструктор класса
+    
     def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
- 
-        # каждый спрайт должен хранить свойство image - изображение
+        
         self.image = transform.scale(image.load(player_image), (65, 65))
         self.speed = player_speed
  
-        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
@@ -34,13 +32,14 @@ class rocket2(GameSprite):
         if keys[k_DOWN] and self.rect.y <5:
             self.rect.y -= self.speed
 
-
+class ball(GameSprite):
+    def update ball
 
 
 
 clock = time.Clock()
 FPS = 60
-#Игровая сцена:
+
 win_width = 700
 win_height = 500
 window = display.set_mode((win_width, win_height))
